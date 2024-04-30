5b8db37db8325c5ea8321ba4d512856e232349f3
Alamin4D <alaminahmed79000@gmail.com>
2024-04-30 09:42:05 +0600

    delete a craft and fetch craft to update

--- index.js
+++ index.js
@@ -73,12 +73,12 @@ async function run() {
         })
 
 
-        // app.delete('/craft/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await craftCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.delete('/craft/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await craftCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
